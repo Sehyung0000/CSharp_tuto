18e8720cd4e2b3a51c0243bdb900db532a23d13a
SeHyung Jung <evanjung@SeHyungs-MacBook-Pro.local>
2026-02-01 23:48:20 -0500

    Module file updates

---
+++ Module5/Unit5/Program.cs
@@ -0,0 +1,28 @@
+﻿/*
+convert the message into a char array
+reverse the chars
+count the o's
+convert it back to a string
+print it out
+*/
+
+
+string message = "The quick brown fox jumps over the lazy dog.";
+
+char[] charMessage = message.ToCharArray();
+Array.Reverse(charMessage);
+
+int letterCount = 0;
+
+foreach (char i in charMessage) 
+{ 
+    if (i == 'o') 
+    { 
+        letterCount++; 
+    } 
+}
+
+string newMessage = new String(charMessage);
+
+Console.WriteLine(newMessage);
+Console.WriteLine($"'o' appears {letterCount} times.");
