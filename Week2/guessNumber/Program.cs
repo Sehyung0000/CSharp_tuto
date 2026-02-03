807ab0b012543c9b2ad599f305945cde70c042e2
SeHyung Jung <evanjung@macbookpro.mynetworksettings.com>
2026-02-03 16:25:44 -0500

    Lab2 Number Guessing

---
+++ Week2/guessNumber/Program.cs
@@ -0,0 +1,50 @@
+using System;
+
+namespace NumberGuess
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("=== Number Guessing Game ===");
+            Console.WriteLine();
+
+            Console.Write("Enter minimum value (a): ");
+            int a = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter maximum value (b): ");
+            int b = Convert.ToInt32(Console.ReadLine());
+
+            Random random = new Random();
+            int answer = random.Next(a, b + 1);
+
+            Console.WriteLine($"\nGuess a number between {a} and {b}!");
+            Console.WriteLine();
+
+            int guess;
+
+            while (true)
+            {
+                Console.Write("Enter your guess: ");
+                guess = Convert.ToInt32(Console.ReadLine());
+
+                if (guess < answer)
+                {
+                    Console.WriteLine("Higher.");
+                }
+                else if (guess > answer)
+                {
+                    Console.WriteLine("Lower.");
+                }
+                else
+                {
+                    Console.WriteLine($"\nCorrect.");
+                    break;
+                }
+                Console.WriteLine();
+            }
+
+            Console.WriteLine("\nGame Over.");
+        }
+    }
+}
